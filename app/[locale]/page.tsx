f61e1c180c72fdcbe67e1c2a6b7d01a324e3a9a6
Cristian Encalada <cris.encalada.camargo@gmail.com>
2025-05-11 23:50:05 -0300

    feat: Redirect root page to /about

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,4 @@
-import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
-import { allBlogs } from 'contentlayer/generated'
-import Main from './Main'
+import { redirect } from 'next/navigation'
 import { LocaleTypes } from './i18n/settings'
 
 type Props = {
@@ -8,7 +6,5 @@ type Props = {
 }
 
 export default async function Page({ params: { locale } }: Props) {
-  const sortedPosts = sortPosts(allBlogs)
-  const posts = allCoreContent(sortedPosts)
-  return <Main posts={posts} params={{ locale: locale }} />
+  redirect(`/${locale}/about`)
 }
